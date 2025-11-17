f66ab44fa2bc5db7fe801723785e6c8353c68a3c
rayssakesia <rayssakesialo@gmail.com>
2025-11-17 10:43:52 -0300

    ...

--- projeto/script.js
+++ projeto/script.js
@@ -2,7 +2,7 @@
 function createUser() {
     const user = {
         name: document.getElementById("name").value.trim(),
-        age: (document.getElementById("age").value),
+        age: document.getElementById("age").value,
         email: document.getElementById("email").value.trim(),
         theme: document.getElementById("userTheme").value,
         format: document.getElementById("userFormat").value,
@@ -13,24 +13,26 @@ function createUser() {
         return ;
     } else {
     
-    const userText = `Usuario criado com sucesso!
-    nome: ${user.name}, 
-    idade:${user.age}, 
-    email:${user.email}, 
-    theme:${user.theme}, 
-    formatação:${user.format}, 
-    idioma:${user.lang}`;
-
-    localStorage.setItem("user", userText);
-    document.getElementById("saida").textContent = userText;
+    localStorage.setItem(user.email, JSON.stringify(user))
     }
+
+}
+
+function Logout (email) {
+    const user = localStorage.getItem(email.value);
+
+    alert(`Volte Sempre !`);
+
+    localStorage.setItem(email, null);
+
+    window.location.href='index.html';
 
 }
 
 function validateUser (user) {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        if (user.name == null || user.name.length < 5) {
-        alert("O nome não pode ser nulo e deve conter pelo menos 5 letras.");
+        if (user.name == null || user.name.length < 3) {
+        alert("O nome não pode ser nulo e deve conter pelo menos 3 letras.");
         return;
     }
      if (user.age < 1 || user.age > 120 || isNaN(user.age)) {
@@ -46,13 +48,62 @@ function validateUser (user) {
 
 }
 
-function getUser (email) {
-    localStorage.getElementById(email);
-    document.getElementById("saida").textContent = userText;
+function validateEmail (email) {
+    const isValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!isValid.test(email.value)) {
+        alert("Digite um e-mail válido.");
+        return;
+    }
+    return true;
+
 }
 
+function goToUserProfile(email) {
+
+    if (!validateEmail(email)) {        
+        return;
+    }
+
+    const user = localStorage.getItem(email.value);
+
+    if (!user) {
+        alert("Usuário não encontrado!");
+        localStorage.setItem("temporary_email", null);
+        window.location.href = "perfil.html";
+    
+
+    } else {
+
+    localStorage.setItem("temporary_email", email.value);
+
+    window.location.href = "perfil.html";
+    }
+
+}
+
+function iniciarPerfil() {
+    const emailTemp = localStorage.getItem("temporary_email");
+    console.log(emailTemp);
+
+    if (!emailTemp) {
+        console.error("Nenhum email temporário encontrado.");
+        return;
+    }
+    const user = JSON.parse(localStorage.getItem(emailTemp));
+
+    console.log(user);
+
+    document.getElementById("email").value = user.email;
+    document.getElementById("name").value = user.name;
+    document.getElementById("age").value = user.age;
+    document.getElementById("userTheme").value = user.theme;
+    document.getElementById("userFormat").value = user.format;
+    document.getElementById("userLang").value = user.lang;
+}
 
 function showPage(p) {
   document.querySelectorAll('.page').forEach(div => div.style.display = 'none');
   document.getElementById('page' + p).style.display = 'block';
 }
+
+object.addEventListener("load", iniciarPerfil);
